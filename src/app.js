4c13d00d78e349e651ee40d0862515a39384897c
EdgarDMen <musicalminor@gmail.com>
2023-03-18 00:44:54 +0000

    Completed

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function getRandomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+function generateDomainName() {
+  const pronouns = ["the", "our", "your"];
+  const adjectives = ["first", "second", "third", "fourth", "fifth"];
+  const nouns = ["hub", "center", "capitol", "centre", "core"];
+  const extensions = [".us", ".com", ".net", ".us", ".io"];
+
+  const pronoun = getRandomElement(pronouns);
+  const adjective = getRandomElement(adjectives);
+  const noun = getRandomElement(nouns);
+  const extension = getRandomElement(extensions);
+
+  return pronoun + adjective + noun + extension;
+}
+
+for (let i = 0; i < 25; i++) {
+  console.log(generateDomainName());
+}
